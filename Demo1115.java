5bec3f3c2c6f40846e870011a7052a0356f1ab4f
jocobzling <924798429@qq.com>
2023-11-15 16:54:47 +0800

    1115

---
+++ Demo1115.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Demo1115 {
+    public int maximizeSum(int[] nums, int k) {
+        int result= 0;
+        int n = nums.length-1;
+        Arrays.sort(nums);
+        while(k!=0){
+            result += nums[n];
+            nums[n] = nums[n]+1;
+            k--;
+        }
+        return result;
+    }
+}
